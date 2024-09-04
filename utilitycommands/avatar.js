038aaf0a9e9f352d98b606bedbd5e4485ccba613
Andreano <87447745+andrxano@users.noreply.github.com>
2024-09-04 11:53:00 +0700

    Update avatar.js

--- utilitycommands/avatar.js
+++ utilitycommands/avatar.js
@@ -1,22 +1,20 @@
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = {
     name: 'avatar',
-    aliases: ["av", "icon"],
-    description: 'Displays the avatar of the user.',
+    aliases:["av"],
+    description: 'Get the avatar of a user.',
     async execute(client, message, args) {
-        let user;
-        if (args.length > 0) {
-            // If an ID or mention is provided
-            user = message.mentions.users.first() || client.users.cache.get(args[0]);
-        } else {
-            // Default to the message author
-            user = message.author;
-        }
-
-        if (!user) {
-            return message.reply("User not found.");
-        }
-
-        const avatarURL = user.displayAvatarURL({ size: 2048, dynamic: true });
-        message.reply(`Avatar of ${user.tag}: ${avatarURL}`);
-    },
+        let user = message.mentions.users.first() || message.author; // Get mentioned user or author if no user mentioned
+        
+        // Create an embed with the user's avatar
+        const embed = new EmbedBuilder()
+            .setTitle(`${user.tag}'s Avatar`)
+            .setImage(user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+            .setColor('#f7931e')
+            .setFooter({ text: 'Requested by ' + message.author.tag, iconURL: message.author.displayAvatarURL({ dynamic: true }) });
+        
+        // Send the embed
+        message.channel.send({ embeds: [embed] });
+    }
 };
