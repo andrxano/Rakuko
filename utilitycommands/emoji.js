a9f539c1edc126a4c1357c191f87560aa4f9564a
Andreano <87447745+andrxano@users.noreply.github.com>
2024-09-09 10:03:08 +0700

    Update emoji.js

--- utilitycommands/emoji.js
+++ utilitycommands/emoji.js
@@ -10,33 +10,24 @@ module.exports = {
         let emoji;
 
         // Check if it's a custom emoji by checking if it contains ID
-        if ((emojiArg.startsWith('<:') || emojiArg.startsWith('<a:')) && emojiArg.endsWith('>')) {
-            // It's a custom emoji (either animated or static)
+        if (emojiArg.startsWith('<:') && emojiArg.endsWith('>')) {
             emoji = emojiArg;
         } else {
             // Convert emoji name to format
             const emojiName = emojiArg.replace(/:/g, '');
-
             // Search for the emoji by name
             const emojiList = client.guilds.cache
-                .flatMap(guild => guild.emojis.cache)
-                .filter(e => e.name === emojiName);
-
+                .map(guild => guild.emojis.cache.find(e => e.name === emojiName))
+                .filter(Boolean);
+            
             if (emojiList.length > 0) {
-                const foundEmoji = emojiList[0];
-
-                // Check if the emoji is animated
-                if (foundEmoji.animated) {
-                    emoji = `<a:${foundEmoji.name}:${foundEmoji.id}>`;  // Animated emoji
-                } else {
-                    emoji = `<:${foundEmoji.name}:${foundEmoji.id}>`;  // Static emoji
-                }
+                emoji = emojiList[0].toString();
             } else {
                 return message.reply('Emoji not found.');
             }
         }
-
+        
         // Send the emoji to the same channel
-        message.channel.send(emoji);
+        message.channel.reply(emoji);
     },
 };
